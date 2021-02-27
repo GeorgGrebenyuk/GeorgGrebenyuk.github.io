810233148fb31aac6d225069b4c60410f6eb6e99
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2021-02-27 15:28:59 +0300

    Update
    Исправил нижнее меню (фиксированное)

---
+++ Navigation/ChronicOtmoroska.mm
@@ -0,0 +1,13 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1614412703916" ID="ID_1346762793" MODIFIED="1614412713272" TEXT="&#x425;&#x440;&#x43e;&#x43d;&#x438;&#x43a;&#x438; &#x43e;&#x442;&#x43c;&#x43e;&#x440;&#x43e;&#x437;&#x43a;&#x430;">
+<node CREATED="1614412717817" ID="ID_1460310925" MODIFIED="1614412732707" POSITION="right" TEXT="&#x422;&#x435;&#x43a;&#x443;&#x449;&#x438;&#x439; &#x434;&#x43d;&#x435;&#x432;&#x43d;&#x438;&#x43a;">
+<node CREATED="1614412734704" ID="ID_561307952" MODIFIED="1614412763374" TEXT="2021-02 &#x421;&#x442;&#x430;&#x432;&#x440;&#x43e;&#x43f;&#x43e;&#x43b;&#x44c;/&#x411;&#x435;&#x448;&#x43f;&#x430;&#x433;&#x438;&#x440;">
+<node CREATED="1614412767497" ID="ID_1972092185" LINK="../posts/Otmorozok/Stavr_02-2021.html#23-02-2021" MODIFIED="1614413067643" TEXT="&#x414;&#x435;&#x43d;&#x44c; 23-02-2021"/>
+<node CREATED="1614412767497" ID="ID_1331852429" LINK="../posts/Otmorozok/Stavr_02-2021.html#24-02-2021" MODIFIED="1614413076228" TEXT="&#x414;&#x435;&#x43d;&#x44c; 24-02-2021"/>
+<node CREATED="1614412767497" ID="ID_1432499214" LINK="../posts/Otmorozok/Stavr_02-2021.html#25-02-2021" MODIFIED="1614413085911" TEXT="&#x414;&#x435;&#x43d;&#x44c; 25-02-2021"/>
+<node CREATED="1614412767497" ID="ID_506442001" LINK="../posts/Otmorozok/Stavr_02-2021.html#26-02-2021" MODIFIED="1614413098349" TEXT="&#x414;&#x435;&#x43d;&#x44c; 26-02-2021"/>
+</node>
+</node>
+</node>
+</map>
